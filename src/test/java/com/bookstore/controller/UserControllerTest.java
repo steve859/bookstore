0f571ad6fc8dfd5bb6d6e7f25504fe2480908d61
steve859 <nguyenhongquan.work@gmail.com>
2025-04-30 22:23:16 +0700

    fix invoices entity

--- src/test/java/com/bookstore/controller/UserControllerTest.java
+++ src/test/java/com/bookstore/controller/UserControllerTest.java
@@ -14,8 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cglib.core.Local;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -31,8 +31,9 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
-    
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -43,41 +44,41 @@ public class UserControllerTest {
     private UserResponse userResponse;
 
     @BeforeEach
-    void initData(){
-        LocalDate today =  LocalDate.now();
+    void initData() {
+        LocalDate today = LocalDate.now();
         userCreationRequest = UserCreationRequest.builder()
-            .username("John Doe")
-            .firstName("John")
-            .lastName("Doe")
-            .password("12345678")
-            .dob(today)
-        .build();
+                .username("John Doe")
+                .firstName("John")
+                .lastName("Doe")
+                .password("12345678")
+                .dob(today)
+                .build();
         userResponse = UserResponse.builder()
-            .id("23k4jk3j3h4")
-            .username("John Doe")
-            .firstName("John")
-            .lastName("Doe")
-            .dob(today)
-        .build();
+                .id("23k4jk3j3h4")
+                .username("John Doe")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(today)
+                .build();
 
     }
-    
+
     @Test
     //
-    void createUser_validRequest_success() throws Exception{
-        //given
+    void createUser_validRequest_success() throws Exception {
+        // given
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         String content = objectMapper.writeValueAsString(userCreationRequest);
         Mockito.when(userService.createUser(ArgumentMatchers.any())).thenReturn(userResponse);
-        //when, then
+        // when, then
         mockMvc.perform(MockMvcRequestBuilders
-            .post("/users")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .content(content))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000));
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value(1000));
 
     }
 }
