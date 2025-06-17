3ac8ef677ee515c4c7b3d76a9a69da154ad2964a
steve859 <nguyenhongquan.work@gmail.com>
2025-06-17 11:05:44 +0700

    fix inventory report

--- src/main/java/com/bookstore/repository/MonthlyInventoryReportRepository.java
+++ src/main/java/com/bookstore/repository/MonthlyInventoryReportRepository.java
@@ -1,16 +1,30 @@
 package com.bookstore.repository;
 
-import com.bookstore.entity.MonthlyDebtReports;
-import com.bookstore.entity.MonthlyInventoryReports;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.entity.MonthlyInventoryReports;
+
 @Repository
 public interface MonthlyInventoryReportRepository extends JpaRepository<MonthlyInventoryReports, Integer> {
     Optional<MonthlyInventoryReports> findByBookIdAndReportMonth(Integer bookId, LocalDate date);
+
     List<MonthlyInventoryReports> findAllByReportMonth(LocalDate date);
+
+    @Query(value = """
+              SELECT * FROM monthly_inventory_reports
+              WHERE EXTRACT(MONTH FROM report_month) = :month
+                AND EXTRACT(YEAR FROM report_month) = :year
+            """, nativeQuery = true)
+    List<MonthlyInventoryReports> findByReportMonthYear(@Param("year") int year, @Param("month") int month);
+
+    @Query("SELECT r FROM MonthlyInventoryReports r WHERE r.reportMonth >= :start AND r.reportMonth < :end")
+    List<MonthlyInventoryReports> findByMonthRange(@Param("start") LocalDate start, @Param("end") LocalDate end);
+
 }
--- src/main/java/com/bookstore/service/MonthlyInventoryReportService.java
+++ src/main/java/com/bookstore/service/MonthlyInventoryReportService.java
@@ -1,68 +1,87 @@
 package com.bookstore.service;
 
-import com.bookstore.dto.response.MonthlyDebtReportResponse;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.bookstore.dto.response.MonthlyInventoryReportResponse;
-import com.bookstore.entity.*;
+import com.bookstore.entity.Books;
+import com.bookstore.entity.MonthlyInventoryReportDetails;
+import com.bookstore.entity.MonthlyInventoryReports;
 import com.bookstore.exception.AppException;
 import com.bookstore.exception.ErrorCode;
 import com.bookstore.mapper.MonthlyInventoryReportMapper;
 import com.bookstore.repository.BookRepository;
 import com.bookstore.repository.MonthlyInventoryReportRepository;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class MonthlyInventoryReportService {
-    @Autowired
-    MonthlyInventoryReportRepository monthlyInventoryReportRepository;
-    @Autowired
-    MonthlyInventoryReportMapper monthlyInventoryReportMapper;
-    @Autowired
-    private BookRepository bookRepository;
+        @Autowired
+        MonthlyInventoryReportRepository monthlyInventoryReportRepository;
+        @Autowired
+        MonthlyInventoryReportMapper monthlyInventoryReportMapper;
+        @Autowired
+        private BookRepository bookRepository;
 
-    public MonthlyInventoryReportResponse createMonthlyInventoryReport(Integer bookId, LocalDate reportDate) {
-        Books book = bookRepository.findById(bookId).orElse(null);
-        MonthlyInventoryReports monthlyInventoryReport = MonthlyInventoryReports.builder()
-                .bookId(Integer.valueOf(bookId))
-                .reportMonth(reportDate)
-                .openingStock(book.getQuantity())
-                .stockIncrease(0)
-                .stockDecrease(0)
-                .closingStock(book.getQuantity())
-                .details(new ArrayList<>())
-                .build();
-        return monthlyInventoryReportMapper.toMonthlyInventoryReportResponse(monthlyInventoryReportRepository.save(monthlyInventoryReport));
-    }
-
-    public List<MonthlyInventoryReportResponse> getMonthlyInventoryReports(LocalDate reportMonth) {
-        return monthlyInventoryReportRepository.findAllByReportMonth(reportMonth.withDayOfMonth(1)).stream().map(monthlyInventoryReportMapper::toMonthlyInventoryReportResponse).toList();
-    }
-
-    public MonthlyInventoryReportResponse updateMonthlyInventoryReport(MonthlyInventoryReportDetails monthlyInventoryReportDetail, int amount, String type) {
-        MonthlyInventoryReports monthlyInventoryReport = monthlyInventoryReportRepository.findByBookIdAndReportMonth(monthlyInventoryReportDetail.getBookId(),monthlyInventoryReportDetail.getReportDate().withDayOfMonth(1))
-                .orElseThrow(() -> new AppException(ErrorCode.MONTHLY_INVENTORY_REPORT_NOT_EXISTED));
-        if(type.equals("Import")){
-            monthlyInventoryReport.setStockIncrease(monthlyInventoryReport.getStockIncrease()+amount);
+        public MonthlyInventoryReportResponse createMonthlyInventoryReport(Integer bookId, LocalDate reportDate) {
+                Books book = bookRepository.findById(bookId).orElse(null);
+                MonthlyInventoryReports monthlyInventoryReport = MonthlyInventoryReports.builder()
+                                .bookId(Integer.valueOf(bookId))
+                                .bookName(book.getName())
+                                .reportMonth(reportDate)
+                                .openingStock(book.getQuantity())
+                                .stockIncrease(0)
+                                .stockDecrease(0)
+                                .closingStock(book.getQuantity())
+                                .details(new ArrayList<>())
+                                .build();
+                return monthlyInventoryReportMapper
+                                .toMonthlyInventoryReportResponse(
+                                                monthlyInventoryReportRepository.save(monthlyInventoryReport));
         }
-        else{
-            monthlyInventoryReport.setStockDecrease(monthlyInventoryReport.getStockDecrease()+amount);
+
+        public List<MonthlyInventoryReportResponse> getMonthlyInventoryReports(LocalDate reportMonth) {
+                log.info(">>> year: {}, month: {}", reportMonth.getYear(), reportMonth.getMonthValue());
+
+                LocalDate start = LocalDate.of(reportMonth.getYear(), reportMonth.getMonth(), 1);
+                LocalDate end = start.plusMonths(1);
+                return monthlyInventoryReportRepository
+                                .findByMonthRange(start, end)
+                                .stream()
+                                .map(monthlyInventoryReportMapper::toMonthlyInventoryReportResponse)
+                                .toList();
         }
-        List<MonthlyInventoryReportDetails> details = monthlyInventoryReport.getDetails();
-        details.add(monthlyInventoryReportDetail);
-        monthlyInventoryReport.setDetails(details);
-        monthlyInventoryReport.setClosingStock(monthlyInventoryReport.getOpeningStock()+monthlyInventoryReport.getStockIncrease()-monthlyInventoryReport.getStockDecrease());
-        return monthlyInventoryReportMapper.toMonthlyInventoryReportResponse(monthlyInventoryReportRepository.save(monthlyInventoryReport));
-    }
+
+        public MonthlyInventoryReportResponse updateMonthlyInventoryReport(
+                        MonthlyInventoryReportDetails monthlyInventoryReportDetail, int amount, String type) {
+                MonthlyInventoryReports monthlyInventoryReport = monthlyInventoryReportRepository
+                                .findByBookIdAndReportMonth(monthlyInventoryReportDetail.getBookId(),
+                                                monthlyInventoryReportDetail.getReportDate().withDayOfMonth(1))
+                                .orElseThrow(() -> new AppException(ErrorCode.MONTHLY_INVENTORY_REPORT_NOT_EXISTED));
+                if (type.equals("Import")) {
+                        monthlyInventoryReport.setStockIncrease(monthlyInventoryReport.getStockIncrease() + amount);
+                } else {
+                        monthlyInventoryReport.setStockDecrease(monthlyInventoryReport.getStockDecrease() + amount);
+                }
+                List<MonthlyInventoryReportDetails> details = monthlyInventoryReport.getDetails();
+                details.add(monthlyInventoryReportDetail);
+                monthlyInventoryReport.setDetails(details);
+                monthlyInventoryReport.setClosingStock(monthlyInventoryReport.getOpeningStock()
+                                + monthlyInventoryReport.getStockIncrease()
+                                - monthlyInventoryReport.getStockDecrease());
+                return monthlyInventoryReportMapper
+                                .toMonthlyInventoryReportResponse(
+                                                monthlyInventoryReportRepository.save(monthlyInventoryReport));
+        }
 }
